865026f4929bee6c1a6246809bb566129a691247
alperrkilic <balper78645@hotmail.com>
2023-02-04 01:00:22 +0300

    Drum Kit Project Introduction

---
+++ Frontend Development/Javascript/Advanced Javascript and DOM manipulation/Higher Order Functions and Passing Functions as Arguments/index.js
@@ -0,0 +1,24 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+    return operator(num1, num2);
+}
+
+let x = calculator(4, 5, add);
+console.log(x);
+let y = calculator(4, 5, multiply);
+console.log(y);
