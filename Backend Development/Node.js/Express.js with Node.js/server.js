91e4a1aa96e6e9a77d037c7f93fd3e41f7b2905e
alperrkilic <balper78645@hotmail.com>
2023-02-06 20:55:10 +0300

    Server-Intro Finish

--- Backend Development/Node.js/Express.js with Node.js/server.js
+++ Backend Development/Node.js/Express.js with Node.js/server.js
@@ -4,10 +4,26 @@ const express = require("express");
 
 const app = express();
 
+//Routes
+
 app.get("/", function (request, response) {
     // console.log(request);
     response.send("<h1>Hello World!</h1>");
 });
+
+app.get("/contact", function (req, res) {
+    res.send("Contact me at: example@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+    res.send("My name is Alper and I love code");
+});
+
+app.get("/hobbies", function (req, res) {
+    res.send("I love playing computer games");
+});
+
+//Starting server
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
