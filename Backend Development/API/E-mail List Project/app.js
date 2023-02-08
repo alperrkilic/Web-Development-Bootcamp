55ffe19dba41ac4d4f6e3ecc06b21a0f66d3ca11
alperrkilic <balper78645@hotmail.com>
2023-02-08 16:48:23 +0300

    Deploying Server

--- Backend Development/API/E-mail List Project/app.js
+++ Backend Development/API/E-mail List Project/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static(__dirname + "/"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000");
 });
 
